82a0121a823f546f30b64e7665067d8857f777ae
Kakyire <47930771+Kakyire@users.noreply.github.com>
2021-07-14 12:54:07 +0000

    Implemented Adaptive UI

--- Dice Roller/ViewController.swift
+++ Dice Roller/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rightDice: UIImageView!
+    @IBOutlet weak var leftDice: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+changeDice()    }
+
+  
+    
+    @IBAction func onRollButtonPressed(_ sender: UIButton) {
+        changeDice()
     }
-
-
+    
+    func changeDice(){
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        leftDice.image = diceArray.randomElement()
+        rightDice.image = diceArray.randomElement()
+        
+    }
 }
 
